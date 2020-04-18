5ee787631aacc8e517f3f577ced7f9476cf3d118
8bitbadger <littlebear101@gmail.com>
2020-04-18 15:05:03 +0000

    Update

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -12,7 +12,7 @@ public class Main : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-systemState
+
     }
 
     public void ChangeSystemState(SystemState newState)
